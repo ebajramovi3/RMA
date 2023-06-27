6a15f9dc88d8858bfa07792bd244af700c5ab462
ebajramovi3 <ebajramovi3@etf.unsa.ba>
2023-06-27 16:16:23 +0200

    UI added

--- app/src/main/java/ba/etf/rma23/projekat/GameDetailsFragment.kt
+++ app/src/main/java/ba/etf/rma23/projekat/GameDetailsFragment.kt
@@ -1,18 +1,22 @@
 package ba.etf.rma23.projekat
 
-import android.content.Context
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageView
+import android.widget.RatingBar
 import android.widget.TextView
+import androidx.core.os.bundleOf
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import ba.etf.rma23.projekat.data.repositories.AccountGamesRepository
+import ba.etf.rma23.projekat.data.repositories.GameReview
+import ba.etf.rma23.projekat.data.repositories.GameReviewsRepository
 import ba.etf.rma23.projekat.data.repositories.GamesRepository
 import ba.unsa.etf.rma.spirala1.R
 import com.squareup.picasso.Picasso
@@ -34,9 +38,12 @@ class GameDetailsFragment : Fragment() {
     private lateinit var description: TextView
     private lateinit var impressionsView: RecyclerView
     private lateinit var impressionsAdapter: UserImpressionsListAdapter
-    private lateinit var impressions: List<UserImpression>
+    private var impressions: List<UserImpression> = emptyList()
     private lateinit var addButton: Button
     private lateinit var removeButton: Button
+    private lateinit var addImpression: Button
+    private lateinit var reviewEditText: EditText
+    private lateinit var ratingBarImpression: RatingBar
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -55,10 +62,13 @@ class GameDetailsFragment : Fragment() {
         genre = view.findViewById(R.id.genre_textview)
         addButton = view.findViewById(R.id.add_button)
         removeButton = view.findViewById(R.id.remove_button)
+        addImpression = view.findViewById(R.id.send_impressions)
+        reviewEditText = view.findViewById(R.id.review_edittext)
+        ratingBarImpression = view.findViewById(R.id.rating_bar_impression)
 
         impressionsView = view.findViewById(R.id.user_impressions_list)
         val layoutManager = LinearLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL, false)
-        impressionsView.layoutManager = layoutManager
+        impressionsView.layoutManager = HeightWrappingLayoutManager(requireContext(), LinearLayoutManager.HORIZONTAL)
         impressionsAdapter = UserImpressionsListAdapter(listOf())
         impressionsView.adapter = impressionsAdapter
         game = GameData.getGame(arguments?.getInt("game_id", 0)!!)
@@ -71,14 +81,59 @@ class GameDetailsFragment : Fragment() {
             }
         }
 
-        impressions = game.userImpressions.sortedByDescending { it.timestamp }
-        populateDetails()
+        val scope = CoroutineScope(Job() + Dispatchers.Main)
+        scope.launch {
+            var impressionsFromServer = GameReviewsRepository.getReviewsForGame(game.id)
 
+            impressionsFromServer.forEach { gameReview ->
+                addReview(gameReview)
+            }
+            impressions.sortedByDescending { it.timestamp }
+            impressionsAdapter.updateUserImpressions(impressions)
+        }
+
+            var rating: Int? = null;
+            ratingBarImpression.setOnRatingBarChangeListener { _, newRating, _  ->
+                rating = newRating.toInt();
+            }
+
+            addImpression.setOnClickListener {
+                val scope = CoroutineScope(Job() + Dispatchers.Main)
+                scope.launch {
+                    var reviewText: String? = reviewEditText.text.toString()
+                    if(reviewText == "")
+                        reviewText = null
+
+                    var review = GameReview(
+                        rating,
+                        reviewText,
+                        game.id,
+                        true,
+                        "",
+                        System.currentTimeMillis().toString()
+                    )
+                    GameReviewsRepository.sendReview(
+                        requireContext(), review
+                    )
+
+                    addReview(review)
+                    impressionsAdapter.updateUserImpressions(impressions)
+                }
+
+                var navController = findNavController()
+                val id = navController.currentDestination?.id
+                navController.popBackStack(id!!,true)
+                navController.navigate(R.id.gameDetailsItem, bundleOf("game_id" to game.id))
+
+            }
+
+        populateDetails()
         addButton.setOnClickListener {
             val scope = CoroutineScope(Job() + Dispatchers.IO)
             scope.launch {
                 AccountGamesRepository.saveGame(game)
                 GameData.favoriteGames.add(game)
+
             }
             addButton.isEnabled = false
             removeButton.isEnabled = true
@@ -97,6 +152,22 @@ class GameDetailsFragment : Fragment() {
         return view
     }
 
+    private fun addReview(gameReview: GameReview) {
+        if (gameReview.review != null && gameReview.review != "")
+            impressions += UserReview(
+                gameReview.student!!,
+                gameReview.timestamp!!.toLong(), gameReview.review!!
+            )
+        if (gameReview.rating != null)
+            impressions += UserRating(
+                gameReview.student!!,
+                gameReview.timestamp!!.toLong(),
+                gameReview.rating!!.toDouble()
+            )
+
+        impressions += game.userImpressions
+    }
+
     private fun populateDetails() {
 
         title.text = game.title
@@ -109,8 +180,7 @@ class GameDetailsFragment : Fragment() {
         description.text = game.description
 
         val imageUrl = game.coverImage
-        Picasso.get().load("https:"+imageUrl).fit().into(coverImage);
-
+        Picasso.get().load("https:" + imageUrl).fit().into(coverImage);
 
         impressionsAdapter.updateUserImpressions(impressions)
     }
